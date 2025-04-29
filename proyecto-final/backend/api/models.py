633945a48fa34b517ed54c08c417dea8897961e4
diegovhdev <diego.valencia.dev@gmail.com>
2025-04-28 19:28:13 -0500

    backend del proyecto final

--- proyecto-final/backend/api/models.py
+++ proyecto-final/backend/api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from rest_framework.fields import DateField
 
-
 # Create your models here.
 class Libro(models.Model):
     titulo = models.CharField(max_length=100)
@@ -12,4 +11,11 @@ class Libro(models.Model):
     def __str__(self):
         return self.titulo
 
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=100)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
 
